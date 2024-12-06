8c168966744c54a0f724078db9b68ecbbdbf5bd1
Naparota <i.naparota@fujitsu.com>
2024-12-06 14:06:46 +0900

    modified to not write location details if chars exceed 32767

--- src/main/Parser.java
+++ src/main/Parser.java
@@ -39,18 +39,18 @@ public class Parser {
 
 		LocalDateTime startTime = LocalDateTime.now();
 
-		fileLocation = args[0];
-		templatePath = args[1];
-		outputPath = args[2];
-		includeLocation = args[3];
-		wordFrequency = 0;
+//		fileLocation = args[0];
+//		templatePath = args[1];
+//		outputPath = args[2];
+//		includeLocation = args[3];
+//		wordFrequency = 0;
 
 		// for testing
-//		fileLocation = "C:\\SMARTMETER\\fy24\\04_word_search\\textparser\\files";
-//		templatePath = "C:\\SMARTMETER\\fy24\\04_word_search\\textparser\\template\\textparser.xlsx";
-//		outputPath = "C:\\SMARTMETER\\fy24\\04_word_search\\textparser\\output";
-//		includeLocation = "yes";
-//		wordFrequency = 0;
+		fileLocation = "C:\\SMARTMETER\\fy24\\04_word_search\\textparser\\files";
+		templatePath = "C:\\SMARTMETER\\fy24\\04_word_search\\textparser\\template\\textparser.xlsx";
+		outputPath = "C:\\SMARTMETER\\fy24\\04_word_search\\textparser\\output";
+		includeLocation = "yes";
+		wordFrequency = 0;
 
 		Helper helper = new Helper();
 
@@ -114,10 +114,15 @@ public class Parser {
 								break;
 							}
 
+							String tempKey = "ファイル名 : " + file.getFileName() + "\n" + "シート : " + hmSheetOfWord.toString();
+							if (tempKey.length() > 32767) { // character limit of each cell in excel
+								tempKey = "ファイル名 : " + file.getFileName();
+							}
+
 							if (!hmSheetOfWord.isEmpty() && includeLocation.toLowerCase().equals("yes")) {
-								hmOccurence.put("ファイル名 : " + file.getFileName() + "\n" + "シート : " + hmSheetOfWord.toString(), wordFrequency);
+								hmOccurence.put(tempKey, wordFrequency);
 							} else {
-								hmOccurence.put("ファイル名 : " + file.getFileName(), wordFrequency);
+								hmOccurence.put(tempKey, wordFrequency);
 							}
 
 						} else {
